8d542f034867828b32b92e085ecd04671888a164
nesibe <nesibeozkan85@gmail.com>
2022-01-14 10:54:37 -0600

    commit4

---
+++ src/day39_Recap/shapeTask/Circle.java
@@ -0,0 +1,47 @@
+package day39_Recap.shapeTask;
+
+public class Circle extends Shape {//onemli extends yapmadan once instancelari yaz get ve set yap sonra extends yap
+    private double radius;
+    public static double pi=3.14;
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius<=0){
+            System.out.println("Invalid radius");
+            System.exit(1);
+        }
+        this.radius = radius;
+    }
+//constructor
+    public Circle( double radius) {
+        super("Circle");
+        setRadius(radius);
+    }
+
+
+    @Override
+    public double area() {
+        return pi*radius*radius;
+
+    }
+
+    @Override
+    public double perimeter() {
+       return 2*pi*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter()+ '\'' +
+                '}';
+    }
+
+
+}
