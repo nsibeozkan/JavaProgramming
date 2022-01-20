ea26ea2c9c40d6cf3480f0595469fa0e38c90e7f
nesibe <nesibeozkan85@gmail.com>
2022-01-20 09:43:56 -0600

    commit

---
+++ officeHour/practice_01_19_2022/ObjectNum.java
@@ -0,0 +1,27 @@
+package practice_01_19_2022;
+
+public class ObjectNum {
+    public static void main(String[] args) {
+        ObjectNum on1 = new ObjectNum(
+
+        );
+        ObjectNum on2 = on1;
+        ObjectNum on3 ;
+        on3 = new ObjectNum();
+        ObjectNum on4 = on3;
+        on4 = new ObjectNum();
+        //System.out.println(on4);
+        //System.out.println(on3);
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
