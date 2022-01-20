2d43f057920b1dd0cd118c4b8ccb95248b722d0c
nesibe <nesibeozkan85@gmail.com>
2022-01-20 10:02:29 -0600

    commit

---
+++ src/utilities/AccessModifiers_Test3.java
@@ -0,0 +1,12 @@
+package utilities;
+
+import day40_FinalKeyword.ProtectedAccessModifier;
+
+public class AccessModifiers_Test3 {
+
+    public static void main(String[] args) {
+       // System.out.println(ProtectedAccessModifier.name1);//default is not visible outside of package
+
+       // System.out.println(ProtectedAccessModifier.name2);//protected  is not always  visible outside of package;
+    }
+}
